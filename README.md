8cbb0bece202164c51b7c10d1f37d4ab4f74cf34
Daniel Miladinov <dmiladinov@wingspan.com>
2013-11-12 00:30:52 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# project_euler_clojure
+
+A Clojure library designed to document my solutions to the [Project Euler][1] problems.
+
+## License
+
+Copyright © 2013 Daniel Miladinov
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
+
+[1]: http://projecteuler.net/problems
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject project_euler_clojure "0.1.0-SNAPSHOT"
+  :description "My Project Euler Solutions in Clojure"
+  :url "https://github.com/danielmiladinov/project-euler-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
